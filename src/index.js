d6e0bf0f0382688f2e8deb41ebe8b31b7f3c9278
Shagun22 <pruthi.shagun@gmail.com>
2019-01-23 12:44:45 +0530

    Uploading code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/searchbar.js';
+import VideoList from './components/video_list.js';
+import YTSearch from 'youtube-api-search';
+import VideoDetail from './components/video_detail.js';
+import _ from 'lodash';
 
-// Create a component that produces some HTML.
-const App = () => {
-  return <div>My First React Page !</div>
+const API_KEY = 'AIzaSyAoXDf1VEBfGKAi954GOmO9XNu5P6dbzAE';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      videos : [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboard');
+
+  }
+
+  videoSearch(term){
+    YTSearch({key:API_KEY,term:term},(videos) => {
+      console.log(videos);
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+
+      });
+    });
+  }
+
+  render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 // Take this HTML and put it in DOM.Pass an instance of a class in ReactDOM.
